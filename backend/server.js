923c0948cc932a026ea1108415db2a4b982167c7
SereyvatanaUng <sereyvatanau@gmail.com>
2023-10-31 22:34:38 +0700

    MongoDB Atlas and Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,36 @@
 require("dotenv").config();
 
 const express = require("express");
+const workoutRoutes = require("./routes/workout.js");
+const mongoose = require("mongoose");
 
 // express app
 const app = express();
 
 // middleware
+app.use(express.json());
+
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
 // routes
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to the app" });
-});
+app.use("/api/workouts", workoutRoutes);
 
-port = process.env.PORT;
+// connect to database
+db = process.env.DATABASE_URL;
+mongoose
+  .connect(db)
+  .then(() => {
+    console.log("DB connected successful");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // listen for requests
+port = process.env.PORT;
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
