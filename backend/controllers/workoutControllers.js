5be13aaac18ed38e1a726cb1a5c5e7bc2787f085
SereyvatanaUng <sereyvatanau@gmail.com>
2023-11-01 15:03:14 +0700

    Controller

---
+++ backend/controllers/workoutControllers.js
@@ -0,0 +1,104 @@
+const Workout = require("../models/workoutModel");
+
+// get all workouts
+exports.getAllWorkouts = async (req, res) => {
+  try {
+    const workouts = await Workout.find();
+
+    res.status(200).json({
+      status: "success",
+      result: workouts.length,
+      data: {
+        workouts,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+};
+
+// get a single workout
+exports.getWorkout = async (req, res) => {
+  try {
+    const workout = await Workout.findById(req.params.id);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        workout,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+};
+
+// create new workout
+exports.createWorkout = async (req, res) => {
+  try {
+    const newWorkout = await Workout.create(req.body);
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        newWorkout,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+};
+
+// delete a workout
+exports.deleteWorkout = async (req, res) => {
+  try {
+    await Workout.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+};
+
+// update a workout
+exports.updateWorkout = async (req, res) => {
+  try {
+    const updateWorkout = await Workout.findByIdAndUpdate(
+      req.params.id,
+      {
+        ...req.body,
+      },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        updateWorkout,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+};
--- backend/routes/workout.js
+++ backend/routes/workout.js
@@ -1,45 +1,18 @@
 const express = require("express");
-const Workout = require("../models/workoutModel");
+const workoutControllers = require("../controllers/workoutControllers");
 
 const router = express.Router();
 
-// GET all workouts
-router.get("/", (req, res) => {
-  res.json({ message: "GET all workouts" });
-});
+router
+  .route("/")
+  .get(workoutControllers.getAllWorkouts)
+  .post(workoutControllers.createWorkout);
 
 // GET a single workout
-router.get("/:id", (req, res) => {
-  res.json({ message: "GET a single workout" });
-});
-
-// POST a new workout
-router.post("/", async (req, res) => {
-  try {
-    const workout = await Workout.create(req.body);
-
-    res.status(201).json({
-      status: "success",
-      data: {
-        data: workout,
-      },
-    });
-  } catch (error) {
-    res.status(400).json({
-      status: "fail",
-      message: error.message,
-    });
-  }
-});
-
-// DELETE a workout
-router.delete("/:id", (req, res) => {
-  res.json({ message: "DELETE a workout" });
-});
-
-// UPDATE a workout
-router.patch("/:id", (req, res) => {
-  res.json({ message: "UPDATE a workout" });
-});
+router
+  .route("/:id")
+  .get(workoutControllers.getWorkout)
+  .delete(workoutControllers.deleteWorkout)
+  .patch(workoutControllers.updateWorkout);
 
 module.exports = router;
