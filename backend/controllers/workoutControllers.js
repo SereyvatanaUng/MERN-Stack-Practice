3ca2a739639115c006b6a9ee89c513c90150478e
SereyvatanaUng <sereyvatanau@gmail.com>
2023-11-02 20:03:34 +0700

    Deleting Data

--- backend/controllers/workoutControllers.js
+++ backend/controllers/workoutControllers.js
@@ -63,7 +63,7 @@ exports.deleteWorkout = async (req, res) => {
   try {
     await Workout.findByIdAndDelete(req.params.id);
 
-    res.status(204).json({
+    res.status(200).json({
       status: "success",
       data: null,
     });
--- frontend/src/components/WorkoutDetails.jsx
+++ frontend/src/components/WorkoutDetails.jsx
@@ -1,4 +1,22 @@
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+
 function WorkoutDetails({ workout }) {
+  const { dispatch } = useWorkoutsContext;
+
+  const handleClick = async () => {
+    const response = await fetch(
+      "http://localhost:4000/api/workouts/" + workout._id,
+      { method: "DELETE" }
+    );
+    console.log(response);
+    console.log(response.json);
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: json });
+    }
+  };
+
   return (
     <div className="workout-details">
       <h4>{workout.title}</h4>
@@ -11,6 +29,7 @@ function WorkoutDetails({ workout }) {
         {workout.reps}
       </p>
       <p>{workout.createdAt}</p>
+      <span onClick={handleClick}>delete</span>
     </div>
   );
 }
